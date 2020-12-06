bf23c2393ea4466c93cea6d17ceae3e1c0b85cbc
acaulkin <acaulkin@umich.edu>
2020-12-05 23:14:34 -0500

    Setup basic framework for files; two functions need to be built as of now: the function for tracking xp, and the function that manages leveling/level ups.

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,17 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { xpTrack, levelUp } from './xpSystem';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "rpcode" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('rpcode.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
+	
+	// Turning on xp tracking
+	let disposable = vscode.commands.registerCommand('rpcode.xpTrack', () => {
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from RPCode!');
+		vscode.window.showInformationMessage(xpTrack());
+		vscode.window.showInformationMessage(levelUp());
 	});
-
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
+// this method is called when the extension is deactivated
 export function deactivate() {}
---
+++ src/xpSystem.ts
@@ -0,0 +1,9 @@
+// Function for tracking xp
+export function xpTrack() {
+    return 'Xp Track Enabled!';
+}
+
+// Function for leveling up
+export function levelUp() {
+    return 'Level Up!';
+}
