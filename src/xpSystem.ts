9bffe086c0d2e39483125d8617aea7d25340daa5
acaulkin <acaulkin@umich.edu>
2020-12-06 10:49:48 -0500

    Comments/formatting.

--- src/xpSystem.ts
+++ src/xpSystem.ts
@@ -1,14 +1,13 @@
-
 export class User {
     constructor() {
         
     }
 
-  
+    // Call below method when user has turned xp tracking on or off
     public xpTrack() {
         return 'XP Tracking Enabled!';
     } 
-
+    // Call below method when user has leveled up
     public levelUp() {
         return 'Level Up!';
     }
