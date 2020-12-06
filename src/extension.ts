76f900dd622a1f555f0ae6329a898de8b1028003
acaulkin <acaulkin@umich.edu>
2020-12-06 12:05:24 -0500

    Formatting/documentation.

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,17 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "rpcode" is now active!');
+	console.log('RPCode is now active!');
 	
 	// Turning on xp tracking
 	vscode.commands.registerCommand('rpcode.xpTrack', () => {
-
-
 		// Detecting change event in current editor
 		vscode.workspace.onDidChangeTextDocument(changeEvent => {
-			// Outputting what file is changed in the debug console
 			console.log(`File Changed: ${changeEvent.document.uri}`);
 			for (const change of changeEvent.contentChanges) {
-				// Output Text being added/changed in the debug console
 				 console.log(change.text); 
 			}
 		});
-		
-
 	});
 }
 
